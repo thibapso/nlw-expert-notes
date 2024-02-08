4f53a169b626679f12dff7a4556689e44f8e9359
Thiago Soares <thibapso@gmail.com>
2024-02-07 22:48:05 -0300

    alterando button -> submit
    O botão não estava adicionando novos cards.

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -63,7 +63,7 @@ export function NewNoteCard() {
                                 )}
                             </div>
 
-                            <button type='button' className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-note font-medium hover:bg-lime-500'>
+                            <button type='submit' className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-note font-medium hover:bg-lime-500'>
                                 Salvar nota
                             </button>
                         </form>
